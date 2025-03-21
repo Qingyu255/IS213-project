3f1bea1d025f6a9670f195dc736914ee25e3396b
LeoZhengKai <leozhengkai@gmail.com>
2025-03-21 18:03:31 +0800

    amendment

--- frontend/src/constants/backend-routes.ts
+++ frontend/src/constants/backend-routes.ts
@@ -2,5 +2,5 @@ export const BACKEND_ROUTES = {
     userManagementServiceUrl: process.env.NEXT_PUBLIC_USER_MANAGEMENT_SERVICE_API_BASE_URL ?? "",
     createEventServiceUrl: process.env.NEXT_PUBLIC_CREATE_EVENTS_SERVICE_API_BASE_URL ?? "http://localhost:8070",
     eventsService: process.env.NEXT_PUBLIC_EVENTS_SERVICE_API_BASE_URL ?? "http://localhost:8001",
-    billingService: process.env.NEXT_PUBLIC_EVENTS_SERVICE_API_BASE_URL ?? "http://localhost:5001",
+    billingService: process.env.NEXT_PUBLIC_BILLING_SERVICE_API_BASE_URL ?? "http://localhost:5001",
 }
