0c1d90d7b4383f64d1aff365315abc154a8322d0
JoelWangg <joel.wang.03@gmail.com>
2025-03-29 16:11:21 +0800

    ceated refund composite, added auth for events ms

--- services/eventsService/src/api/routes/events.py
+++ services/eventsService/src/api/routes/events.py
@@ -13,15 +13,16 @@ from src.services.event_service import (
     delete_event,
 )
 from src.db.connection import get_db 
+from src.core.auth import get_current_user_id, validate_token
 
 router = APIRouter(prefix="/events", tags=["Events"])
 
 @router.post("/create", response_model=EventCreateResponse)
-async def create_event_endpoint(event: EventCreate, db: AsyncSession = Depends(get_db)):
+async def create_event_endpoint(event: EventCreate, db: AsyncSession = Depends(get_db), user_id: str = Depends(get_current_user_id) ):
     """
     Create a new event.
     """
-    return await create_event(event, db)
+    return await create_event(event, db, user_id)
 
 @router.get("/{event_id}", response_model=EventRead)
 async def get_event_by_id_endpoint(event_id: UUID4, db: AsyncSession = Depends(get_db)):
@@ -38,18 +39,18 @@ async def get_all_events_endpoint(skip: int = 0, limit: int = 10, db: AsyncSessi
     return await get_all_events(db, skip, limit)
 
 @router.put("/update/{event_id}", response_model=EventRead)
-async def update_event_endpoint(event_id: UUID4, event: EventUpdate, db: AsyncSession = Depends(get_db)):
+async def update_event_endpoint(event_id: UUID4, event: EventUpdate, db: AsyncSession = Depends(get_db), user_id: str = Depends(get_current_user_id) ):
     """
     Update an existing event.
     """
-    return await update_event(event_id, event, db)
+    return await update_event(event_id, event, db, user_id)
 
 @router.delete("/delete/{event_id}")
-async def delete_event_endpoint(event_id: UUID4, db: AsyncSession = Depends(get_db)):
+async def delete_event_endpoint(event_id: UUID4, db: AsyncSession = Depends(get_db), user_id: str = Depends(get_current_user_id) ):
     """
     Delete an event by its ID.
     """
-    return await delete_event(event_id, db)
+    return await delete_event(event_id, db, user_id)
 
 
 
